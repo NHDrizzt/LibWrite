23feed54c69cedf75ce8d2ba8e6404c44e51d7d6
Rokol <biel_s.carvalho2@hotmail.com>
2021-04-01 15:15:10 -0300

    PathVariable com ID

--- src/main/java/com/example/controllers/EventoController.java
+++ src/main/java/com/example/controllers/EventoController.java
@@ -2,6 +2,7 @@ package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,7 +31,16 @@ public class EventoController {
 	public ModelAndView listaEventos() {
 		ModelAndView mv = new ModelAndView("index"); //modelandview para passar a view e a model em um unico returno
 		Iterable<Evento> eventos = er.findAll(); //busca no banco
-		mv.addObject("evento", eventos);// view e model
+		mv.addObject("evento", eventos);// view e model "evento q ta na view e vai substituir pelos dados q estao no banco"
+
+		return mv;
+	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+		Evento evento =er.findByCodigo(codigo); //invoca o metodo no repository do crud para procurar o evento com o determinado codigo e guarda-lo na var evento.
+		ModelAndView mv = new ModelAndView("detalhesEvento");
+		mv.addObject("evento", evento);
 		return mv;
 	}
 }
--- src/main/java/com/example/repository/EventoRepository.java
+++ src/main/java/com/example/repository/EventoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.models.Evento;
 
 public interface EventoRepository extends CrudRepository<Evento, String>{
-
+	Evento findByCodigo(long codigo);//procura codigo que recebeu no parametro no banco 
 }
